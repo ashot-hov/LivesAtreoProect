b11954aa6553d7ea34659ce33877e8875c8ba287
LivesAtreoDevelopment <35258732+LivesAtreoDevelopment@users.noreply.github.com>
2018-02-02 19:42:17 +0400

    RGBMonstrer

---
+++ javaScript/RGBMonster/script.js
@@ -0,0 +1,7 @@
+function rgb(e){
+	sr.innerHTML = r.value;
+	sg.innerHTML = g.value;
+	sb.innerHTML = b.value;
+	palitra.style.background = "rgb(" + r.value + "," + g.value + "," + b.value + ")";
+	palitra_code.value = "rgb(" + r.value + "," + g.value + "," + b.value + ")";
+}
