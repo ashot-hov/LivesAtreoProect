d321766ed5ab4401605bcd19729ae7539e322b21
Ashot Hvhannisyan <35258732+LivesAtreoDevelopment@users.noreply.github.com>
2018-05-22 02:06:51 +0400

    App update

--- javaScript/klavaOrg/script.js
+++ javaScript/klavaOrg/script.js
@@ -1,5 +1,6 @@
-const input = document.querySelector("input"),
-	  val = ["hello", "well", "bell", "Ashot", "big", "placeholder", "personal", "kiss", "brother", "single", "shift", "asus", "lives", "atreo", "samsung", "xiaomi", "meizu", "lol", "link", "border", ""]
+
+const input = document.querySelector("#input"),
+	  val = ["hello", "well", "bell", "Ashot", "closer", "close", "meddle", "run", "user", "big", "timer", "set", "interval", "placeholder", "float", "display", "zero", "up", "down", "hot", "cool", "col", "key", "mouse", "element", "good", "get", "bad", "main", "eco", "clock", "click", "align", "center", "right", "left", "personal", "kiss", "brother", "single", "shift", "asus", "lives", "atreo", "samsung", "xiaomi", "meizu", "lol", "link", "border", ""]
 
 let rnad = valRandom(0, val.length);
 
@@ -8,8 +9,18 @@ function valRandom(max, min){
 	input.placeholder = val[Math.floor((Math.random() * foo))];
 }
 
+let arr = [];
+
 input.addEventListener("input", function cancelBubble(e){
 	if(this.value == input.placeholder){
+		arr.push(this.value);
+		console.log(arr);  // just chek 
+
+		let elem = document.createElement("div");
+		count.appendChild(elem);
+		elem.classList.add("elem");
+		elem.innerText = this.value;
+
 		out.innerText = "ok";
 		if(out.innerText = "ok"){
 			setTimeout(() =>{
@@ -19,9 +30,58 @@ input.addEventListener("input", function cancelBubble(e){
 		out.style.color = "green";
 		setTimeout(() =>{
 			this.value = "";
-			// console.log(this);
 			valRandom();
 		}, 700)
 	}
 
-}) 
+	
+
+})
+
+
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", start);
+
+function start(){
+	time();
+}
+
+function time(){
+	let num = document.getElementById("outline"),
+	porgres = document.getElementById("line"),
+	i = setTieme.value;
+
+	console.log(setTieme.value);
+
+	lol = setInterval(function(){ line() }, 1000);
+
+	function line() {
+		num.innerText = i;
+   		i--;
+
+		if(i <= -1){
+			clearInterval(lol);
+		}
+
+		function minus(){
+
+			if(i <= 0){
+				input.style.display = "none";
+				all.innerText = `words count: ${arr.length}`;
+			}else{
+				input.style.display = "block";
+			}
+		}
+
+		minus();
+	}
+
+}
+
+
+/*
+
+Сделать возможность включать таймер и счиать колличество ввенных символов. 
+
+*/
