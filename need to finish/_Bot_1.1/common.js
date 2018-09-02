2844ebd5e93252407d441aff294284be5520644a
Ashot <35258732+LivesAtreoDevelopment@users.noreply.github.com>
2018-09-02 13:06:19 +0400

    Creating Bot using Node.js 1.6
    Creating Bot using Node.js and vanilla JavaScript

--- need to finish/_Bot_1.1/common.js
+++ need to finish/_Bot_1.1/common.js
@@ -1,39 +1,47 @@
 let botConfig = require("./bot-config.json"),
 	userJSON = require("./user-config.json");
 
-
 function Bot(options){
 	if(options.userAsk == userSay){
-		// User asked
-		let foo = userJSON.greet.length;
-		console.log(userJSON.greet[Math.floor((Math.random() * foo))]);
-
-		// Bot answered
 		this.start = (form, to) =>{
+
+			// User asked
+			let foo = userJSON.greet.length;
+			console.log(userJSON.greet[Math.floor((Math.random() * foo))]);
+
+
+			// Bot answered
 			setTimeout(()=>{
 				console.log("...");
 				setTimeout(()=>{
 					let foo = botConfig.greet.length;
 					console.log(botConfig.greet[Math.floor((Math.random() * foo))]);
+
+					console.log("__________________________");
 				}, 700)
 			}, 1000)
+
 		}		
 	}
 
 }
 
-
 let userSay;
-	for(let userWord = 0; userWord < userJSON.greet; userWord++){
-		userSay = userWord;
-	}
+for(let userWord = 0; userWord < userJSON.greet; userWord++){
+	userSay = userWord;
+}
 
 let first = new Bot({
 	userAsk: userSay
 })
 
-first.start();
+// first.start();
+
+setInterval(()=>{
+	first.start();
+}, 2500);
 
 
 
-// TODO: Create a new json file for user
+
+// TODO: Make new object with items for talking theme
